f9487d7f0bbb2fbf6e116a3d5e97ff45024fa81b
algojedi <vicbanwait@hotmail.com>
2020-01-14 20:33:31 -0500

    still working in progress

--- routes/auth.js
+++ routes/auth.js
@@ -22,16 +22,19 @@ router.post('/login', (req, res, next) => {
                     if (doMatch) {
                         req.session.isLoggedIn = true;
                         req.session.user = user;
-                        return req.session.save(err => {
-                            console.log(err);
-                            res.redirect('/home'); //after (un)successfully saving
+                        req.session.save(err => {
+                            console.log(err ? err : 'session saved');
+                            //err is undefined if no error
                         });
+                        console.log('brcypt match! session isLogged and user saved and user sent');
+                        return res.send(user);
+                        //res.redirect('/home'); //after (un)successfully saving
                     }
                     return res.status(400).send('incorrect email or password');
                 })
                 .catch(err => {
                     console.log(err);
-                    res.redirect('/home'); //error with bcrypt process
+                    //res.redirect('/home'); //error with bcrypt process
                 });
         })
         .catch(err => console.log(err));
@@ -54,14 +57,24 @@ router.post('/logout', (req, res, next) => {
     });
 });
 
+router.get('/checkauth', (req, res, next) => {
+    console.log('req user? ', ('user' in req));
+    console.log('req.seshion in checkauth: ', req.session);
+    //console.log('in checkauth route')
+    if(! 'session' in req ) {
+        res.end();
+    } 
+    console.log('req session user exists? ', ('user' in req.session));
+    return ('user' in req.session) ? res.send(user) : res.send('no user found');
+})
 
 router.post('/register', async (req, res, next) => {
-    const { name, email, password, confirmPassword } = req.body;
+    const { name, email, password } = req.body;
     
     try { 
         const userDoc = await User.findOne({ email }) 
         if (userDoc) {
-            return res.status(400).send('user already exists');
+            return res.status(400).send({message: 'user already exists'});
         }
     }
     catch(err) { 
