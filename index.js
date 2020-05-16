abb97b15639180cbb683715932f6f3814d228167
algojedi <vibanwait@hotmail.com>
2020-05-16 20:18:11 +0000

    rate limiting added

--- index.js
+++ index.js
@@ -6,18 +6,19 @@ const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth");
 const wordRoutes = require("./routes/words");
 const redisClient = require("./redis");
+const rateLimit = require("express-rate-limit");
 
 require("dotenv").config();
 
-const MONGO_URI = `mongodb+srv://${process.env.DB_UN}:${process.env.DB_PW}@cluster0-ohht9.azure.mongodb.net/test?retryWrites=true&w=majority`;
+const MONGO_URI = `mongodb+srv://${process.env.DB_UN}:${process.env.DB_PW}@cluster0-ohht9.azure.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 //removed from uri: ?retryWrites=true&w=majority
 
 const app = express();
 
 if (process.env.NODE_ENV === "production") {
-  console.log("Running in production");
+	console.log("Running in production");
 } else {
-  console.log(process.env.NODE_ENV);
+	console.log(process.env.NODE_ENV);
 }
 
 app.use(cors());
@@ -25,50 +26,72 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "wordsie", "build")));
 
--app.get("/", function (req, res) {
-  +app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "wordsie", "build", "index.html"));
-  });
- });
+//---------- rate limiting --------------------//
+//TODO: remove all middleware into middleware folder
 
-//enable service worker
-app.get('/servicer-worker.js', (req, res) => {
+let counter = 0; //simple ddos precaution
 
-	res.sendFile(path.resolve(__dirname, 'wordsie', 'build', 'service-worker.js'));
-//res.sendFile(path.resolve(__dirname, '..', 'wordsie', 'build', 'service-worker.js'));
-
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // limit each IP to 100 requests per windowMs
+	message: "You have exceeded the 100 requests in 15 mins limit!",
+	headers: true
 });
 
-if (process.env.NODE_ENV === "production") {
-  console.log("Running in production");
-} else {
-  console.log(process.env.NODE_ENV);
-}
+//  apply to all requests, targetting each user
+app.use(limiter);
+
+const myLimiter = function (req, res, next) {   
+	counter++;
+	console.log("Total requests ", counter);
+	//TODO: remove/reset this later
+	counter > 5000 ? res.json("ummm... something went wrong") : next();
+} 
+//apply to all requests, regardless of user
+app.use(myLimiter);
+
+
+//---------------------------------------
 
 
 //save userid on req object if there's a token
-app.use(function (req, res, next) {
-  const { authorization } = req.headers; //authorization is the token
-  if (!authorization) {
-    console.log("no auth token");
-    return next(); // user will not be able to access all routes
-  } else {
-    //    console.log(authorization)
-    redisClient.get(authorization, (err, reply) => {
-      if (err || !reply) {
-        console.log("issue with token", err);
-        return res.status(400).json("Authorization denied");
-      }
-      //     console.log('reply from redis in middleware', reply)
-      req.userId = JSON.parse(reply);
-      next();
-    });
-  }
+app.use(function(req, res, next) {
+	const { authorization } = req.headers; //authorization is the token
+	if (!authorization) {
+		console.log("no auth token");
+		return next(); // user will not be able to access all routes
+	} else {
+		//    console.log(authorization)
+		redisClient.get(authorization, (err, reply) => {
+			if (err || !reply) {
+				console.log("issue with token", err);
+				return res
+					.status(400)
+					.json("Authorization denied");
+			}
+			//     console.log('reply from redis in middleware', reply)
+			req.userId = JSON.parse(reply);
+			next();
+		});
+	}
 });
 app.use(authRoutes);
 app.use(wordRoutes);
 
+-app.get("/", function(req, res) {
+	+app.get("/*", function(req, res) {
+		res.sendFile(
+			path.join(__dirname, "wordsie", "build", "index.html")
+		);
+	});
+});
 
+//enable service worker
+app.get("/servicer-worker.js", (req, res) => {
+	res.sendFile(
+		path.resolve(__dirname, "wordsie", "build", "service-worker.js")
+	);
+});
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(result => {
@@ -80,4 +103,3 @@ mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 	.catch(err => {
 		console.log(err);
 	});
-
